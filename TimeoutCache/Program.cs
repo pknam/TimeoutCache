c353e66e7ea8db2df279053af9e5ac27816706e7
pknam <pknam.kr@gmail.com>
2016-01-18 10:46:14 +0900

    init

---
+++ TimeoutCache/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimeoutCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // set timeout as 3 sec
+            TimeSpan cacheTimeout = new TimeSpan(0, 0, 3);
+            TimeoutCache<string, int> cache = new TimeoutCache<string, int>(cacheTimeout);
+
+            // set value
+            cache["hihi"] = 10;
+
+            while(true)
+            {
+                Console.WriteLine(cache["hihi"]);
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ TimeoutCache/TimeoutCache.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeoutCache
+{
+    class TimeoutCache<K, V>
+    {
+        class Value
+        {
+            public DateTime creationTime;
+            public V value;
+
+            public Value(DateTime creationTime, V value)
+            {
+                this.creationTime = creationTime;
+                this.value = value;
+            }
+        }
+
+        private TimeSpan _invalidateTimeout;
+        private Dictionary<K, Value> _cache;
+        
+        public TimeoutCache(TimeSpan invalidateTimeout)
+        {
+            _invalidateTimeout = invalidateTimeout;
+            _cache = new Dictionary<K, Value>();
+        }
+
+        public V this[K key]
+        {
+            get
+            {
+                Value v = _cache[key];
+                
+                // cache miss
+                if(v.creationTime.Add(_invalidateTimeout) < DateTime.Now)
+                    return default(V);
+
+                // cache hit
+                return v.value;
+            }
+            set
+            {
+                _cache.Add(key, new Value(DateTime.Now, value));
+            }
+        }
+    }
+}
